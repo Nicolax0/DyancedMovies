1f1a19e78e6cbb1e86211eb932399cf68bad1919
Nicolax0 <DylanHNicolay@gmail.com>
2022-09-13 19:07:04 -0400

    Make array-appending function

--- main.js
+++ main.js
@@ -1,19 +1,11 @@
 const genreList = document.querySelector(".genre-list");
 const movieList = document.querySelector(".movie-list");
+const genreUrl = "https://api.themoviedb.org/3/genre/movie/list?api_key=03b17a1691b771af4f892d87416c2236&language=en-US";
 
-const apiData = {
-    url: "https://api.themoviedb.org/",
-    parameters: "3/genre/tv/list",
-    apiKey: "?api_key=03b17a1691b771af4f892d87416c2236",
-    language: "&language=en-US"
-}
-const {url, parameters, apiKey, language}= apiData;
-const apiUrl = `${url}${parameters}${apiKey}${language}`;
-
-let gSelected = newArray();
+let gChose = [];
 
 // call api to get genres
-fetch(apiUrl)
+fetch(genreUrl)
     .then(res => res.json())
     .then((file) =>
         {file.genres.forEach(
@@ -21,7 +13,7 @@ fetch(apiUrl)
                 let list = document.createElement("li");
                 let space = document.createElement("p");
                 space.setAttribute("target", "_blank");
-                space.addEventListener("click", function(){editList(genreSet.id)});
+                space.addEventListener("click", function(){editList(gChose, genreSet.id)});
                 space.setAttribute("style", "padding-top: 5px;");
                 space.textContent = genreSet.name;
                 list.className = ".genre-list";
@@ -31,6 +23,19 @@ fetch(apiUrl)
         )}
     )
 
-function editList(id){
-    console.log(id);
+function editList(gC, id){
+    // removes id from gChose if exist in gChose, adds if not exist
+    let doesNotExist = true;
+    for (let i = 0; i<gC.length; i++){
+        if (gC[i]==id){
+            gC.splice(i, 1);
+            doesNotExist = false;
+        }
+    }
+    if(doesNotExist)
+        gC.push(id);
+    
+    for (let i = 0; i<gC.length; i++){
+        console.log(gC[i]);
+    }
 }
